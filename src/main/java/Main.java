0e574df11843cf24fbc94d30cf99cc0f41ecf4a3
moises <moiseszandonai1234@gmail.com>
2024-05-06 19:05:14 -0300

    EXERCICIO RESOLVIDO

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Main {
+public class Main
+{
+    public static void main(String[] args) {
+        Romano romano = new Romano("DCCLXXXVIII");
+        System.out.println(romano.toArabic());
+        Arabic arabic = new Arabic(788);
+        System.out.println(arabic.getNumberRomanic());
+
+    }
 }
--- src/main/java/Romano.java
+++ src/main/java/Romano.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Romano {
+
+public class Romano {
+    private String romanicNumber;
+
+    public Romano(String number) {
+        this.romanicNumber = number.toUpperCase();
+        }
+
+
+        public int toArabic() {
+        int result = 0;
+        for (int i = 0 ;i < romanicNumber.length()-1; i++) {
+            char actualNumber = romanicNumber.charAt(i);
+            char nextNumber = romanicNumber.charAt(i + 1);
+            if (getNumberRomanic(actualNumber) >= getNumberRomanic(nextNumber)) {
+            result += getNumberRomanic(actualNumber);
+            } else {
+            result -= getNumberRomanic(actualNumber);
+            }
+        }
+        result += getNumberRomanic(romanicNumber.charAt(romanicNumber.length()-1));
+        return result;
+        }
+
+        public int getNumberRomanic(char number) {
+        if (number == 'V') {
+            return 5;
+        } else if (number == 'X') {
+            return 10;
+        } else if (number == 'L') {
+            return 50;
+        } else if (number == 'C') {
+            return 100;
+        } else if (number == 'D') {
+            return 500;
+        } else if (number == 'M') {
+            return 1000;
+        }
+        return 1;
+    }
 }
